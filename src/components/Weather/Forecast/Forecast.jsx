0f21c561a6596449a736df6a31039f54a7ba94e4
Christian Martinez <chmartinez.cba@gmail.com>
2024-05-12 21:49:45 -0300

    fix: slider arrows

--- src/components/Weather/Forecast/Forecast.jsx
+++ src/components/Weather/Forecast/Forecast.jsx
@@ -2,25 +2,21 @@ import ForecastCard from "./ForecastCard";
 import "./Forecast.css";
 import "../../../styles/slider.css"
 import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import { Icon } from '@iconify-icon/react';
-import { useRef } from "react";
 
 const Forecast = ({ daily, dailyForecast, weekForecast }) => {
-    const slider = useRef(null)
-
     const settings = {
         dots: false,
         infinite: false,
         slidesToShow: 5,
-        arrows:false
     }
 
     return (
         <section className="card forecast__container">
             <h3 className="card__title">{daily ? "Pronóstico del día" : "Pronóstico de la semana"}</h3>
             <section className="slider-container">
-                <Slider className="slider" ref={slider} {...settings}>
+                <Slider className="slider" {...settings}>
                     {
                         daily 
                         ?
@@ -33,8 +29,6 @@ const Forecast = ({ daily, dailyForecast, weekForecast }) => {
                         ))
                     }
                 </Slider>
-                <button className="slider-btn slider-btn--left" onClick={() => slider?.current?.slickPrev()}><Icon icon="iconamoon:arrow-left-6-circle-fill"/></button>
-                <button className="slider-btn slider-btn--right" onClick={() => slider?.current?.slickNext()}><Icon icon="iconamoon:arrow-right-6-circle-fill"/></button>
             </section>
         </section>
     );
