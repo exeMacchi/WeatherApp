19fd7a1415feef1e6dc986f5eaa0e4a3c9be5095
Christian Martinez <chmartinez.cba@gmail.com>
2024-05-20 14:13:11 -0300

    feat: se añade pagina sobre nosotros y componente footer

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -11,25 +11,29 @@ import Login from "./Login/Login";
 import Navbar from "../components/Nav/Navbar";
 import Register from "./Register/Register";
 import Favorites from "./Favorites/Favorites";
+import Footer from "../components/Footer/Footer";
+import About from "./About/About";
 
 const LandingPage = () => {
     const [ bgClass, setBgClass ] = useState("default"); // Controlar el background
     const [ isLogged, setIsLogged] = useState(false); // Controlar la sesión
 
     return (
-        <div className={`bg--${bgClass}`}>
+        <div className={`wrapper bg--${bgClass}`}>
             <header className="container">
                 <Navbar isLogged={isLogged} setIsLogged={setIsLogged}/>
             </header>
-            <main className="container">
+            <main className="container container--main">
                 <Routes>
                     <Route path='/' element={<HomePage setBgClass={setBgClass}/>}/>
                     <Route path="/weather" element={<WeatherPage setBgClass={setBgClass} isLogged={isLogged}/>}/>
                     <Route path="/login" element={<Login setBgClass={setBgClass} setIsLogged={setIsLogged}/>}/>
                     <Route path="/register" element={<Register/>}/>
-                    <Route path="/favorites" element={<Favorites isLogged={isLogged}/>}/>
+                    <Route path="/favorites" element={<Favorites setBgClass={setBgClass} isLogged={isLogged}/>}/>
+                    <Route path="/about" element={<About setBgClass={setBgClass}/>}/>
                 </Routes>
             </main>
+            <Footer/>
         </div>
     );
 }
